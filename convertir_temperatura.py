82a628e154e28c1c73cdbb6503c29099af12f565
Naza2006 <nazario@uamv.edu.ni>
2025-04-02 13:38:47 -0600

    cree la carpeta

---
+++ convertir_temperatura.py
@@ -0,0 +1,11 @@
+grado_f = float(input("Grados Fharenheit: ")) # float = real
+grado_c = ((grado_f - 32) * 5) / 9
+print(grado_c)
+
+# +, -
+r = 2 + 2 - 2
+print(r)
+r = 2 - 2 + 3
+print(r)
+r = 2 - (2 + 3)
+print(r)
